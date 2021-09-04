2565ee1f433f5203912960588319b7ddfb54ad52
Andreas Borowczak <aborowczak@sleibo.de>
2021-09-04 21:26:37 +0200

    Datenbank Tabelle Section erstellt und eingefügt

--- home/views.py
+++ home/views.py
@@ -1,13 +1,15 @@
 from django.http import HttpResponse
 from django.template import loader
-from .models import Section
+from .models import Section, Content
 
 
 def home(request):
     sec = Section.objects.order_by('published_date')[:3]
+    con = Content.objects.order_by('published_date')[:3]
     template = loader.get_template('home/index.html')
-    output = ', '.join([q.title for q in sec])
+    # output = ', '.join([q.title for q in sec])
     context = {
         'Section': sec,
+        'Content': con,
     }
     return HttpResponse(template.render(context, request))
